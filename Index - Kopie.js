cbaa8e94c9d6295a6aac6df5275f1c248feb9945
polito761 <82938368+polito761@users.noreply.github.com>
2021-05-04 18:34:15 +0200

    1st
    the foundation

---
+++ Index - Kopie.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const PREFIX = 't!';
+const TOKEN = 'ODM4MzU4ODQ1OTg1MTI4NDU5.YI58hA.4vNPzpBEQz83B0QPodE7F7sbtA4';
+
+var version = '1.0.1';
+
+bot.on("ready", () => {
+    console.log('der bot ist nun einsatzbereit')
+
+    bot.user.setActivity("t!help", { type: 'PLAYING'}).catch(console.error)
+})
+
+bot.on('message', message =>{
+    let args = message.content.substring(PREFIX.lenght).split(" ");
+
+    switch (args[0]){
+
+
+        case 'help':
+             message.channel.send('es gibt 8 bots und 14 mitglieder!')
+        break;
+
+        case 'ping':
+            message.channel.send('Pong!')
+        break;
+
+        case 'Ping':
+            message.channel.send('Pong!')
+        break;
+
+        case 'clear':
+            if(!args[1])return message.channel.send("Fehler, bitte zweites argument angegeben!")
+            mesagge.channel.bulkDelete(args[1])
+        break;
+        
+        case 'Clear':
+            if(!args[1])return message.channel.send("Fehler, bitte zweites argument angegeben!")
+            mesagge.channel.bulkDelete(args[1])
+        break;
+    }
+})
+
+bot.login(TOKEN) 
